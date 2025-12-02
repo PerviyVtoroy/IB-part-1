f285153169f55261622272530bcf22e9459e4dac
PerviyVtoroy <kostdanil777@gmail.com>
2025-12-02 13:30:51 +0500

    111

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,52 +1,50 @@
+"use client"
+
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@heroui/react";
+import Image from 'next/image';
 
 export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
+	return (
+		<Image
+			src='/logo.png'
+			alt='Logo'
+			width={0}
+			height={0}
+			className='w-[200px] sm:w-[160px] max-w-none'
+		/>
+	)
+}
 
-export default function App() {
-  return (
-    <Navbar>
-      <NavbarBrand>
-        <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link aria-current="page" href="#">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
-  );
+
+export default function Header() {
+  const navItems = [
+    {href: "/", label: "recipes"},
+    {href: "/ingredients", label: "ingredients"},
+    {href: "/about", label: "About us"}
+  ];
+
+return (
+		<Navbar className='h-[6rem]'>
+			<NavbarBrand>
+				<a href='/'>
+					<AcmeLogo />
+				</a>
+			</NavbarBrand>
+			<NavbarContent className='hidden sm:flex gap-4' justify='center'>
+				{navItems.map(item => {
+					return (
+						<NavbarItem key={item.href}>
+							<Link
+								className='text-[18px] sm:text-[16px] lg:text-[18px] font-medium'
+								color='foreground'
+								href={item.href}
+							>
+								{item.label}
+							</Link>
+						</NavbarItem>
+					)
+				})}
+			</NavbarContent>
+		</Navbar>
+	)
 }
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+		<div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+			<main className='text-5xl font-bold text-gray-900 dark:text-white mb-8'>
+		Костиков Данил АПО-22
+			</main>
+		</div>
+	)
+	// return (
+	// 	<main className='min-h-screen'>
+	// 		<HeroSlider />
+	// 	</main>
+	// )
+}
